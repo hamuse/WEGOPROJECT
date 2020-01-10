195f6d0dbb9171a8179cd5253b18ecd4d58633fd
rivery811 <rivery811@gmail.com>
2020-01-10 16:10:19 +0900

    4:10
    주석3

--- src/main/java/com/wego/web/cmm/HomeController.java
+++ src/main/java/com/wego/web/cmm/HomeController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
- * Handles requests for the application home page.adfadsf
+ * Handles requests for the application home page.adfadsfajkdghjahgkadsgafdga
  */  
 @Controller
 public class HomeController {
