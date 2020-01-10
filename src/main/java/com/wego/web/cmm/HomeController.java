50c94ec21365f4ede76894000714b60cc5659cef
hamuse <hamuseco@gmail.com>
2020-01-10 16:27:16 +0900

    test hamuse
    test hamuse

--- src/main/java/com/wego/web/cmm/HomeController.java
+++ src/main/java/com/wego/web/cmm/HomeController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
- * Handles requests for the application home page.garam
+ * Handles requests for the application home page.hamuse
  */  
 @Controller
 public class HomeController {
